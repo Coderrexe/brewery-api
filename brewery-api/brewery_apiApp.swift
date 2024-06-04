9d81748b3ddfb43657ffb4dc5a5d1e9d691e980a
Simba Shi <shizimo@gmail.com>
2024-06-04 10:03:40 +0100

    Initial Commit

---
+++ brewery-api/brewery_apiApp.swift
@@ -0,0 +1,17 @@
+//
+//  brewery_apiApp.swift
+//  brewery-api
+//
+//  Created by Shi, Simba (Coll) on 04/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct brewery_apiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ brewery-apiUITests/brewery_apiUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  brewery_apiUITestsLaunchTests.swift
+//  brewery-apiUITests
+//
+//  Created by Shi, Simba (Coll) on 04/06/2024.
+//
+
+import XCTest
+
+final class brewery_apiUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
