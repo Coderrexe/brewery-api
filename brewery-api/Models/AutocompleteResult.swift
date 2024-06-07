e6701ceb4e74412f7bf305d0ebfb5480f683d3e4
Simba Shi <shizimo@gmail.com>
2024-06-07 15:50:32 +0100

    Fix bug

---
+++ brewery-api/Models/AutocompleteResult.swift
@@ -0,0 +1,8 @@
+//
+//  AutocompleteResult.swift
+//  brewery-api
+//
+//  Created by Shi, Simba (Coll) on 04/06/2024.
+//
+
+import Foundation
---
+++ brewery-api/Views/BreweryDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  BreweryDetailsView.swift
+//  brewery-api
+//
+//  Created by Shi, Simba (Coll) on 04/06/2024.
+//
+
+import SwiftUI
+
+struct BreweryDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BreweryDetailsView()
+}
